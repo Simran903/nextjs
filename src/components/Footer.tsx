d0ecc76b8c53d8bc41e0160dbf7679abd3cbcc1a
Simran <itssimran903@gmail.com>
2024-08-12 14:00:43 +0530

    footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 function Footer() {
     return (
       <footer className="bg-black text-gray-400 py-12">
-          <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-20 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 px-4 sm:px-6 lg:px-8">
           <div>
             <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
             <p className="mb-4">
@@ -51,7 +51,7 @@ function Footer() {
           </div>
           <div>
             <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
-            <div className="flex space-x-4">
+            <div className="flex flex-col">
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
